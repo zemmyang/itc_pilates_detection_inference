cca9f59eee23a5e83ece70afe0bd5790f3fae286
Angeleene (Zemmy) Ang <zemmy.ang@gmail.com>
2021-11-03 02:53:09 +0200

    folder

--- src/Model/VGGModelSetUp.py
+++ src/Model/VGGModelSetUp.py
@@ -11,7 +11,7 @@ def model_reconstruct():
         print("Loading model...")
     else:
         print("Model not found, unzipping from archives...")
-        zip_list = [Path(i) for i in glob(f'{CONF.MODELS_PATH}/21oct.zip.*')]
+        zip_list = [Path(i) for i in glob(f'{CONF.MODELS_PATH}/21oct.h5.zip.*')]
 
         for zipName in zip_list:
             with open(os.path.join(CONF.MODELS_PATH, "21oct.zip"), "ab") as f:
@@ -19,7 +19,7 @@ def model_reconstruct():
                     f.write(z.read())
 
         with zipfile.ZipFile(os.path.join(CONF.MODELS_PATH, "21oct.zip"), "r") as zipObj:
-            zipObj.extractall(CONF.MODELS_PATH)
+            zipObj.extractall(os.path.join(CONF.MODELS_PATH, '21oct.h5'))
 
         [os.remove(file)for file in zip_list]
         os.remove(os.path.join(CONF.MODELS_PATH, "21oct.zip"))
