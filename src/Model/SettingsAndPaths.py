c6159de08982a75d7aa10cbfb63063c2bce2e9b7
Angeleene (Zemmy) Ang <zemmy.ang@gmail.com>
2021-11-09 03:24:17 +0200

    Revert "heroku not making empty folders for files"
    This reverts commit 0464788c

--- src/Model/SettingsAndPaths.py
+++ src/Model/SettingsAndPaths.py
@@ -1,5 +1,4 @@
 import os
-from pathlib import Path
 
 
 TEMPLATE_FOLDER = 'View/templates'
@@ -24,11 +23,6 @@ MODELS_DIRECTORY = 'model_weights'
 FRAMES_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), FRAMES_FOLDER)
 VIDEO_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), DATA_UPLOAD_FOLDER)
 OUT_FRAMES_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), OUT_FOLDER)
+
 MODELS_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), MODELS_DIRECTORY)
 
-
-def create_folders():
-    Path(FRAMES_PATH).mkdir(parents=True, exist_ok=True)
-    Path(VIDEO_FOLDER).mkdir(parents=True, exist_ok=True)
-    Path(OUT_FRAMES_PATH).mkdir(parents=True, exist_ok=True)
-    Path(MODELS_PATH).mkdir(parents=True, exist_ok=True)
